093bc14bfea64915e79ddcf2c4c29f0e6888c4d8
svu <svu>
2004-07-20 23:02:13 +0000

    bash is great but sh is standard - and think optimal, dude

--- rules/merge.sh
+++ rules/merge.sh
@@ -1,19 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 DEST=$1
 shift
-
-lineNo=0
 
 > $DEST
 
 for i in $*; do
   if [ "$i" == "HDR" ] ; then
     echo >> $DEST;
-    lineNo=$((lineNo + 1))
-    head -$lineNo HDR | tail -1 >> $DEST
+    read hdr
+    echo "$hdr" >> $DEST
   else
     cat $i >> $DEST;
   fi
-done
+done < HDR
 
