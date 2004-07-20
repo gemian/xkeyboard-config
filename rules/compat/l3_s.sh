2580c7b3092591b7754ffedd17137661e91c6ac3
svu <svu>
2004-07-20 01:57:06 +0000

    the extra layouts (2-4) should be ADDED using '+' character

--- rules/compat/l3_s.sh
+++ rules/compat/l3_s.sh
@@ -6,5 +6,5 @@ OUTFILE=base.l3_s.part
 > $OUTFILE
 
 awk '{ 
-  printf "  %s		=	%s%%(v[3]):3\n", $1, $2; 
+  printf "  %s		=	+%s%%(v[3]):3\n", $1, $2; 
 }' < layoutRename.lst >> $OUTFILE
--- rules/compat/l4_s.sh
+++ rules/compat/l4_s.sh
@@ -6,5 +6,5 @@ OUTFILE=base.l4_s.part
 > $OUTFILE
 
 awk '{ 
-  printf "  %s		=	%s%%(v[4]):4\n", $1, $2; 
+  printf "  %s		=	+%s%%(v[4]):4\n", $1, $2; 
 }' < layoutRename.lst >> $OUTFILE
