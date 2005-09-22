03718155d5f8c29f1e803c30aefd7621985c6959
svu <svu>
2005-09-22 22:19:58 +0000

    fixing build

--- rules/merge.sh
+++ rules/merge.sh
@@ -3,15 +3,19 @@
 DEST=$1
 shift
 
+if [ -z "$HDR" ]; then
+    HDR="HDR"
+fi
+
 > $DEST
 
 for i in $*; do
-  if [ "$i" == "HDR" ] ; then
+  if [ "$i" == "$HDR" ] || [ "$i" == "HDR" ]; then
     echo >> $DEST;
     read hdr
     echo "$hdr" >> $DEST
   else
     cat $i >> $DEST;
   fi
-done < HDR
+done < $HDR
 
