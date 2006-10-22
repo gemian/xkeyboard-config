a923a4bd1e9273bda4272fe14c78309801a37e20
svu <svu>
2006-10-22 12:31:59 +0000

    some Ruby stuff, just my little toys

---
+++ tests/ruby/find_match.rb
@@ -0,0 +1,30 @@
+require "xkbparser.rb"
+
+basedir = "../.."
+
+parser = Parser.new
+
+allSyms = parser.parse("#{basedir}/symbols/inet")
+
+newSyms = parser.parse(ARGV[0])
+limit = ARGV[1].to_i
+
+newSyms.find_all do | key, value |
+
+  if value.hidden?
+    next
+  end
+
+  puts "Existing xkb_symbols matching #{key}: "
+
+  sorted = allSyms.match_symbols(value,limit).sort_by do | symsName, diff |
+    sprintf "%03d_%s", diff.size, symsName
+  end
+
+  sorted.find_all do | symsName, diff |
+    puts "  #{symsName} (difference #{diff.size})-> #{diff}"
+  end
+
+end
+
+
