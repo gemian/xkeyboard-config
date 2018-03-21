41c115d6774ac7aaa6cfc2c7c1d113e63b0ea351
Adam Boardman <adamboardman@gmail.com>
2018-03-21 14:41:16 +0000

    Dissableing maintiner mode as we are not distributing the maintainer files

--- autogen.sh
+++ autogen.sh
@@ -10,4 +10,4 @@ autopoint --force
 AUTOPOINT='intltoolize --automake --copy' autoreconf -v --install --force || exit 1
 
 cd $ORIGDIR || exit $?
-$srcdir/configure --enable-maintainer-mode "$@"
+$srcdir/configure "$@"
